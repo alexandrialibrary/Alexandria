f08f25e2d4be195e88a3ad2aa914dca893ea7976
Hawk Weisman <hawk@meteorcodelabs.com>
2015-05-18 21:03:19 -0400

    Embettered Book schema

--- src/main/scala/me/hawkweisman/alexandria/model/Book.scala
+++ src/main/scala/me/hawkweisman/alexandria/model/Book.scala
@@ -2,7 +2,7 @@ package me.hawkweisman.alexandria
 package model
 
 case class Book (
-    isbn: Long, // ISBNs are unique identifiers for a book in the database
+    isbn: ISBN, // ISBNs are unique identifiers for a book in the database
     title: String,
-    author: String
+    authors: Seq[String],
 )
