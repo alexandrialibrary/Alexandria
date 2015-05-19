b25081ed24cd3501852183e162ebc8e291194382
Hawk Weisman <hawk@meteorcodelabs.com>
2015-05-18 21:15:43 -0400

    Add Author case class
    This is so we can organize authors by last name

--- src/main/scala/me/hawkweisman/alexandria/model/Book.scala
+++ src/main/scala/me/hawkweisman/alexandria/model/Book.scala
@@ -1,8 +1,21 @@
 package me.hawkweisman.alexandria
 package model
 
+case class Author(
+  firstName: String,
+  lastName: String,
+  middleName: String
+  )
+
 case class Book (
     isbn: ISBN, // ISBNs are unique identifiers for a book in the database
     title: String,
-    authors: Seq[String],
-)
+    subtitle: Option[String],
+    authors: Seq[Author],
+    pages: Int,
+    deweys: Seq[Float],
+    checkedOut: Bool // todo: check out to a person
+) {
+  def checkOut: Book = Book(isbn,title,subtitle,authors,pages,deweys,true)
+  def checkIn: Book = Book(isbn,title,subtitle,authors,pages,deweys,false)
+}
