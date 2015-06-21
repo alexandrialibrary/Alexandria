54ad466fbc71cb6151b56c99e690f133b8b4b40e
Hawk Weisman <hawk@meteorcodelabs.com>
2015-06-21 15:29:15 -0400

    Add logging to bootstrap

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -6,25 +6,34 @@ import org.scalatra._
 import javax.servlet.ServletContext
 
 import com.mchange.v2.c3p0.ComboPooledDataSource
+import com.typesafe.scalalogging.LazyLogging
+
 import slick.driver.JdbcDriver.api._
 
-class ScalatraBootstrap extends LifeCycle {
+class ScalatraBootstrap extends LifeCycle
+  with LazyLogging {
 
   val cpds = new ComboPooledDataSource
-  //logger.info("Created c3p0 connection pool")
+  logger info "Created c3p0 connection pool"
 
   implicit val swagger = new AlexandriaSwagger
+  logger info "Created Swagger API data"
 
   override def init(context: ServletContext) {
     val db = Database.forDataSource(cpds)     // create a Database which uses the DataSource
+    logger info "Created database"
     // mount the admin controller
     context mount (AdminController(db), "/admin/*")
+    logger info "Mounted AdminController at /admin/"
     // mount the API swagger control
     context mount (new ResourcesController, "/api/docs")
+    logger info "Mounted ResourcesController at /api/docs"
     // mount the API controller
     context mount (APIController(db), "/api/*")
+    logger info "Mounted APIController at /api/"
     // mount the application
     context mount (AppController(db), "/*") // TODO: eventually this won't require DB access
+    logger info "Mounted AppController at /"
   }
 
   private def closeDbConnection() {
