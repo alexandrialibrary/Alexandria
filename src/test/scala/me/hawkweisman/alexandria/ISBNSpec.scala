8c5c7541ce021b2b9a2d4d0eedaf3966e177b215
Hawk Weisman <hawk@meteorcodelabs.com>
2015-06-22 23:42:57 -0400

    Removed get from specs This should make Codacy happier with me

--- src/test/scala/me/hawkweisman/alexandria/ISBNSpec.scala
+++ src/test/scala/me/hawkweisman/alexandria/ISBNSpec.scala
@@ -1,18 +1,21 @@
-import org.scalatest.{Matchers,WordSpec}
+import org.scalatest.{Matchers,WordSpec,TryValues}
 
 import me.hawkweisman.alexandria.model.ISBN
 
-class ISBNSpec extends WordSpec with Matchers {
+class ISBNSpec extends WordSpec
+  with Matchers
+  with TryValues {
+
   "The ISBN class" when {
     "validating ISBN-13 numbers" should {
       "handle a sample ISBN-13 number correctly" in {
-        val isbn = ISBN.parse("9780306406157").get
+        val isbn = ISBN.parse("9780306406157").success.value
         isbn.isbn13CheckValue shouldEqual 7
       }
     }
     "validating ISBN-10 numbers" should {
       "handle a sample ISBN-10 number correctly" in {
-        val isbn = ISBN.parse("0306406152").get
+        val isbn = ISBN.parse("0306406152").success.value
         isbn.isbn10CheckValue shouldEqual 2
       }
     }
